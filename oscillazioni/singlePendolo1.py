c287d5250a3acea3908ca15e40367ea10c17e63b
metallo455445 <leonardimatteo2006@gmail.com>
2026-03-18 09:47:53 +0100

    creati programmi per il pendolo singolo e pendolo smorzato

---
+++ oscillazioni/singlePendolo1.py
@@ -0,0 +1,102 @@
+#py oscillazioni/singlePendolo.py oscillazioni/esperienzaOscillazioni/run\ pendolo\ singolo/0309_000091/0309_000091_data.txt 70 oscillazioni/esperienzaOscillazioni/run\ pendolo\ singolo/0309_000092/0309_000092_data.txt 60
+import numpy as np                          #numeracci
+from matplotlib import pyplot as plt        #grafici
+from scipy.optimize import curve_fit        #fit
+from scipy.signal import find_peaks
+import matplotlib as mpl                    #pgf
+import sys
+
+#####################################################################################################################################################
+#blocco preambolo pgf
+#decommentare fino alla riga 16 per scaricare i file in .pgf Inoltre è necessario il comando plt.savefig("pendolo.pgf") a fine documento
+# mpl.use("pgf")
+
+# plt.rcParams.update({
+#     "font.family": "serif",     
+#     "text.usetex": True,
+#     "pgf.rcfonts": False,
+# })
+#####################################################################################################################################################
+
+# Blocco variabili passate come argomento
+if len(sys.argv) == 5:
+    percorsoData1 = sys.argv[1]
+    skippedrows1 = int(sys.argv[2])
+    percorsoData2 = sys.argv[3]
+    skippedrows2 = int(sys.argv[4])
+else:
+    # Valori di default di sicurezza se lo script è eseguito senza argomenti
+    print("Attenzione: argomenti mancanti, uso i valori di default.")
+    percorsoData1 = "data1.csv"   
+    skippedrows1 = 0
+    percorsoData2 = "data2.csv"
+    skippedrows2 = 0
+
+#####################################################################################################################################################
+
+# --- SET DI DATI 1 ---
+data1 = np.loadtxt(percorsoData1, delimiter=',', skiprows=skippedrows1)
+
+mask_A1 = data1[:, 0] == 4.0  # Tutte le righe dove il Pin è 4
+mask_B1 = data1[:, 0] == 5.0  # Tutte le righe dove il Pin è 5
+
+tempiA1 = data1[mask_A1, 1]
+dataA1 = data1[mask_A1, 2]
+
+tempiB1 = data1[mask_B1, 1]
+dataB1 = data1[mask_B1, 2]
+
+indici_massimi1, _ = find_peaks(dataA1) 
+tempi_massimi1 = tempiA1[indici_massimi1]
+posizioni_massimi1 = dataA1[indici_massimi1]
+
+# --- PLOT 1 ---
+plt.figure(1)
+plt.plot(tempiA1, dataA1, label="Dati Sensore A (Set 1)")
+plt.plot(tempi_massimi1, posizioni_massimi1, "x", color="red", label="Massimi (Set 1)") 
+
+# --- SET DI DATI 2 ---
+# CORRETTO: ricaricavi percorsoData1! Ora carica il 2.
+data2 = np.loadtxt(percorsoData2, delimiter=',', skiprows=skippedrows2)
+
+mask_A2 = data2[:, 0] == 4.0  
+mask_B2 = data2[:, 0] == 5.0  
+
+tempiA2 = data2[mask_A2, 1]
+dataA2 = data2[mask_A2, 2]
+
+tempiB2 = data2[mask_B2, 1]
+dataB2 = data2[mask_B2, 2]
+
+indici_massimi2, _ = find_peaks(dataA2) 
+tempi_massimi2 = tempiA2[indici_massimi2]
+posizioni_massimi2 = dataA2[indici_massimi2]
+
+# --- PLOT 2 --- (sulla stessa figura)
+plt.figure(2)
+plt.plot(tempiA2, dataA2, label="Dati Sensore A (Set 2)", alpha=0.7) # Alpha per renderlo un po' trasparente se si sovrappone
+plt.plot(tempi_massimi2, posizioni_massimi2, "o", color="orange", label="Massimi (Set 2)") 
+plt.legend()
+
+
+# --- CALCOLO DELTA TEMPI E OMEGA ---
+# Usiamo np.diff per calcolare la differenza tra elementi adiacenti.
+# Lo facciamo separatamente per evitare di contare il salto temporale tra la fine del Set 1 e l'inizio del Set 2
+deltaTempo1 = np.diff(tempi_massimi1)
+deltaTempo2 = np.diff(tempi_massimi2)
+
+# Uniamo i delta tempo in un unico array
+deltaTempo_TOT = np.concatenate((deltaTempo1, deltaTempo2))
+
+print("Vettore dei delta tempo (Periodi):")
+print(deltaTempo_TOT)
+
+# Calcolo Omega
+omega_media = (2 * np.pi) / np.mean(deltaTempo_TOT)
+print(f"Omega media = {omega_media:.4f} rad/s")
+
+#####################################################################################################################################################
+plt.show()
+# se si vuole usare plt.show commentare la prossima riga
+# plt.savefig('nome.pgf')
+#####################################################################################################################################################
---
+++ oscillazioni/singlePendoloSmorzato.py
@@ -0,0 +1,115 @@
+#py oscillazioni/singlePendoloSmorzato.py oscillazioni/esperienzaOscillazioni/run\ pendolo\ smorzato/0309_000095/0309_000095_data.txt 60 1 2.0
+import numpy as np                          #numeracci
+from matplotlib import pyplot as plt        #grafici
+from scipy.optimize import curve_fit        #fit
+from scipy.signal import find_peaks
+import matplotlib as mpl                    #pgf
+import sys
+
+#####################################################################################################################################################
+#blocco preambolo pgf
+#decommentare fino alla riga 16 per scaricare i file in .pgf Inoltre è necessario il comando plt.savefig("pendolo.pgf") a fine documento
+# mpl.use("pgf")
+
+# plt.rcParams.update({
+#     "font.family": "serif",     
+#     "text.usetex": True,
+#     "pgf.rcfonts": False,
+# })
+#####################################################################################################################################################
+
+#blocco variabili passate come argomento
+if len(sys.argv) == 5:
+    percorsoData1 = sys.argv[1]
+    skippedrows1 = int(sys.argv[2])
+    tailRows = int(sys.argv[3])
+    prominenza = float(sys.argv[4])
+    # percorsoData2 = sys.argv[3]
+    # skippedrows2 = int(sys.argv[4])
+else:
+    # Valori di default di sicurezza se lo script è eseguito senza argomenti
+    print("Attenzione: argomenti mancanti, uso i valori di default.")
+    percorsoData1 = "data1.csv"   
+    skippedrows1 = 0
+    # percorsoData2 = "data2.csv"
+    # skippedrows2 = 0
+
+#####################################################################################################################################################
+
+def exp(t, lamda, b, offset):
+    return offset + np.exp(-lamda*t) * b 
+
+errorBalda = 1
+
+data = np.loadtxt(percorsoData1, delimiter=',', skiprows=skippedrows1)
+
+tempiA = np.empty(int(len(data)/2))
+tempiB = np.empty(int(len(data)/2))
+dataA = np.empty(int(len(data)/2))
+dataB = np.empty(int(len(data)/2))
+
+mask_A = data[:, 0] == 4.0  # Tutte le righe dove il Pin è 4
+mask_B = data[:, 0] == 5.0  # Tutte le righe dove il Pin è 5
+
+tempiA = data[mask_A, 1]
+dataA = data[mask_A, 2]
+
+tempiB = data[mask_B, 1]
+dataB = data[mask_B, 2]
+
+indici_massimi, _ = find_peaks(dataA, prominence=prominenza) 
+tempi_massimi = tempiA[indici_massimi]
+posizioni_massimi = dataA[indici_massimi]
+
+plt.figure(1)
+plt.plot(tempiA, dataA, label="Dati Sensore A")
+plt.plot(tempi_massimi, posizioni_massimi, "x", color="red", label="Massimi") 
+
+
+#gli ultimi valori selezionati da find_peaks non sono solo piscchi, elimina gli ultimi n valori
+max_selected_data = posizioni_massimi[:-tailRows]
+max_selected_times = tempi_massimi[:-tailRows]
+
+#imposto a 0 il tempo iniziale
+tempi_fit = max_selected_times - max_selected_times[0]
+
+#sistemo i valor iniziali
+offset_iniziale = max_selected_data[-1]     #valore più piccolo che dovrei trovare
+amplificazione_iniziale = max_selected_data[0] - offset_iniziale
+lamdaStimato = 0.01
+
+popt, pcov = curve_fit(exp, tempi_fit, max_selected_data, 
+                       p0=[lamdaStimato, amplificazione_iniziale, offset_iniziale],
+                       bounds=(0, np.inf))
+lamda0 = popt[0]
+bi = popt[1]
+offset0 = popt[2]
+
+perr = np.sqrt(np.diag(pcov))
+print(f"{perr}")
+
+#calcolo residui e chi2
+residui = (max_selected_data - exp(tempi_fit, lamda0, bi, offset0)) / errorBalda 
+chi2 = np.sum(np.power(residui,2))
+
+#plot del fit 
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[3, 1])
+ax1.errorbar(tempi_fit, max_selected_data,errorBalda, xerr=None, fmt="o", label="Massimi selezionati")
+ax1.plot(tempi_fit, exp(tempi_fit, lamda0, bi, offset0), label=rf"$\chi^2/dof = {chi2:.2f}/{len(tempi_fit)-len(popt)}$")
+ax1.legend()
+
+#plot residui
+ax2.axhline(0, color='black', linestyle='dashed')
+ax2.errorbar(tempi_fit, residui, errorBalda, fmt='o')
+ax2.set_xlabel("tempo [s]")
+ax2.set_ylabel("Residui [sigma]")
+ax2.grid(ls='dashed')
+print(f"chi2: {chi2}")
+
+# plt.figure(2)
+# plt.plot(tempiB, dataB)
+#####################################################################################################################################################
+plt.show()
+#se si vuole usare plt.show commentare la prossima riga
+#plt.savefig('nome.pgf')
+#####################################################################################################################################################
